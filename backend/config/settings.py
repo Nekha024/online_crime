bc7ee603774621cc70264cd87c67b2b4ba980720
Adarsh <adarshm.off@gmail.com>
2026-09-06 17:14:21 +0530

    updated readme

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -37,9 +37,24 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    
+    #apps
+    "core",
+    #react_connection
+    
+    "rest_framework",
+    "corsheaders",
+    
+]
+
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:3000",
+    "http://127.0.0.1:3000",
+
 ]
 
 MIDDLEWARE = [
+    "corsheaders.middleware.CorsMiddleware",
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
